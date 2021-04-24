56806b7b0fbddd36cb8f9e8211a449f4b6c2f710
jlandercy <jeanlandercy@live.com>
2021-04-24 18:01:40 +0200

    Update decode.js

--- js/decode.js
+++ js/decode.js
@@ -3,7 +3,7 @@ function Decode(port, bytes) {
   return {
     "error": (bytes[0] & 0x01),
     "fixed": (bytes[0] & 0x02) >> 1,
-    "time": bytes[1] | (bytes[2] << 8) | (bytes[3] << 16),
+    "gpstime": bytes[1] | (bytes[2] << 8) | (bytes[3] << 16),
     "longitude": (bytes[5] | (bytes[6] << 8) | (bytes[7] << 16) | (bytes[8] << 24))/1000000.,
     "latitude": (bytes[9] | (bytes[10] << 8) | (bytes[11] << 16) | (bytes[12] << 24))/1000000.,
     "height": bytes[13] | (bytes[14] << 8),
